3348ea6023535c6c9711b025a1cc57a72dccb16d
duchn0908 <duc.hn0908@gmail.com>
2024-05-03 15:58:14 +0700

    video3-mvc

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import initWebRoutes from "./routes/web";
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 configViewEngine(app);
 
