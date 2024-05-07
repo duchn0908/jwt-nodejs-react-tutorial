4e1249bea2801cfcf0b12ac09b933a4cdd53ab5d
duchn0908 <duc.hn0908@gmail.com>
2024-05-07 16:38:52 +0700

    video 5 create new user

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
+// import bodyParser from "body-parser";
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+//config view Engine
+configViewEngine(app)
 
-configViewEngine(app);
 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//init web routes
 initWebRoutes(app);
 
 
