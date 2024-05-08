a783abc8b51efe31d32d2c2eabc504a57a478ed5
duchn0908 <duc.hn0908@gmail.com>
2024-05-08 14:43:55 +0700

    5.1 hash password

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
-// import bodyParser from "body-parser";
-const bodyParser = require('body-parser');
+import bodyParser from "body-parser";
+// const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
